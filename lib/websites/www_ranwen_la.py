02d841ef76025e1858371bea2a61975de43f4446
tsaiminghan <minghan.tsai@gmail.com>
2022-04-29 23:22:40 +0800

    Add files via upload
    remove advertisement

--- lib/websites/www_ranwen_la.py
+++ lib/websites/www_ranwen_la.py
@@ -1,10 +1,11 @@
+from bs4 import BeautifulSoup
 from ._sample import NovelDL as Base
 
 NAME = '燃文小說'
 NETLOC = 'www.ranwen.la'
 REGEX = fr'(?<={NETLOC}/)(files/article/\d+/\d+)'
 
-# OK in 2020/10/07
+# OK in 2022/04/29
 # https://www.ranwen.la/files/article/89/89698/
 # https://www.ranwen.la/files/article/89/89698/99664594.html
 
@@ -13,4 +14,10 @@ class NovelDL(Base):
     name = NAME
     netloc = NETLOC
     regex = REGEX
+    @staticmethod
+    def content_filter(text):
+        content = BeautifulSoup(text, 'lxml').find(id='content')
+        for tag in content.select('a, div'):
+            tag.extract()
+        return content.get_text()
 
